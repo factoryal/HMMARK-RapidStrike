e84e8ccdf79ca4e9988d02cdf5fd119f19a8b514
vkdlsvnt <vkdlsvnt@naver.com>
2020-06-21 02:31:08 +0900

    pin mode added

--- hmmark-rapidstrike/hmmark-rapidstrike.ino
+++ hmmark-rapidstrike/hmmark-rapidstrike.ino
@@ -7,6 +7,14 @@ SoftwareSerial BT(PIN_BT_RX, PIN_BT_TX);
 void setup() {
     Serial.begin(9600);
     BT.begin(9600);
+    Serial.println("service ready.");
+
+    pinMode(PIN_BTN_1, INPUT_PULLUP);
+    pinMode(PIN_BTN_2, INPUT_PULLUP);
+    pinMode(PIN_LED_1, OUTPUT);
+    pinMode(PIN_LED_2, OUTPUT);
+    pinMode(PIN_BUZ, OUTPUT);
+
 }
 
 void loop() {
