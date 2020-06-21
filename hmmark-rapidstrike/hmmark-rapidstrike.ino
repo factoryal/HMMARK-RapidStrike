3e34c2db3d6d5f5e7b08a0fe19cf37a1f7144844
vkdlsvnt <vkdlsvnt@naver.com>
2020-06-21 13:34:04 +0900

    lcd_i2c library added

--- hmmark-rapidstrike/hmmark-rapidstrike.ino
+++ hmmark-rapidstrike/hmmark-rapidstrike.ino
@@ -4,6 +4,18 @@
 
 SoftwareSerial BT(PIN_BT_RX, PIN_BT_TX);
 
+void keyEvent(uint8_t id, uint8_t event) {
+    switch(event) {
+        case EVENT::PRESS:
+        digitalWrite(id + 5, HIGH);
+        break;
+
+        case EVENT::RELEASE:
+        digitalWrite(id + 5, LOW);
+        break;
+    }
+}
+
 void setup() {
     Serial.begin(9600);
     BT.begin(9600);
@@ -15,8 +27,14 @@ void setup() {
     pinMode(PIN_LED_2, OUTPUT);
     pinMode(PIN_BUZ, OUTPUT);
 
+    btn[0].setEventCallback(keyEvent, 0);
+    btn[1].setEventCallback(keyEvent, 1);
+
+    delay(2000);
 }
 
 void loop() {
     
+    btn[0].update();
+    btn[1].update();
 }
