1bb846ce9214cb7f3e6c85e4584dd37f93cbcf1a
vkdlsvnt <vkdlsvnt@naver.com>
2020-06-21 13:43:56 +0900

    Create CountController.h

---
+++ hmmark-rapidstrike/CountController.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Arduino.h"
+
+class CountController {
+private:
+    int32_t cnt = 0;
+
+public:
+    CountController();
+
+    void increaseOne() {
+        cnt += 1;
+    }
+
+    void increase(int32_t n) {
+        cnt += n;
+    }
+
+    void set(int32_t n) {
+        cnt = n;
+    }
+
+    int32_t get() {
+        return cnt;
+    }
+
+};
